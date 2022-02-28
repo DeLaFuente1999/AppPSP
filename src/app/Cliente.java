3501af34fd84c655bc1bd9efa1913ae7f8ee4065
alan7rivas <alan7rivas@gmail.com>
2022-02-28 10:42:01 +0100

    Ficheros y Server

--- src/app/Cliente.java
+++ src/app/Cliente.java
@@ -1,9 +1,8 @@
 package app;
 
 public class Cliente {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
 
-        System.out.println("hola mundo");
+    public static void main(String[] args) {
+
     }
 }
--- src/app/Servidor.java
+++ src/app/Servidor.java
@@ -1,4 +1,19 @@
 package app;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Servidor {
+
+    private static final int PORT = 9500;
+    private ServerSocket server = null;
+    private Socket client = null;
+    private DataOutputStream dos = null;
+    private DataInputStream dis = null;
+    public static void main(String[] args) {
+
+
+    }
 }
